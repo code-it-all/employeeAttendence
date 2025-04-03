19b4e8ba6ba241961fb739e68ea18c1e964b62b1
Vrishti Sharma <riyavrishti@gmail.com>
2025-04-03 15:48:30 +0530

    password checker update

--- PasswordStrengthChecker/main.py
+++ PasswordStrengthChecker/main.py
@@ -6,13 +6,28 @@ print('Inclusion of numbers and special characters.')
 
 password = input("Enter Password: ")
 import re
+# if len(password) >= 8:
+#     if re.search(r'[A-Z]', password) and re.search(r'[a-z]', password) and re.search(r'[0-9]', password):
+#         if re.search(r'[!@#$%^&*]', password):
+#             print("Strong Password")
+#     else:
+#         print('Medium Password')
+# else:
+#     print('Weak Password')
+
 if len(password) >= 8:
-    if re.search(r'[A-Z]', password) and re.search(r'[a-z]', password) and re.search(r'[0-9]', password):
-        if re.search(r'[!@#$%^&*]', password):
-            print("Strong Password")
+    if re.search(r'[A-Z]', password):
+        if re.search(r'[a-z]', password):
+            if re.search(r'[0-9]', password):
+                if re.search(r'[!@#$%^&*]', password):
+                    print("Strong Password")
+                else:
+                    print("Medium Password")
+            else:
+                print("Medium Password")
+        else:
+            print("Medium Password")
     else:
-        print('Medium Password')
+        print("Weak Password")
 else:
-    print('Weak Password')
-
-
+    print("Weak Password")
