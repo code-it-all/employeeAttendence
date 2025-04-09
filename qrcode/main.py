aba51a7ef4a2ef79803fbcf3e41e4dea6fe43ef9
Vrishti Sharma <riyavrishti@gmail.com>
2025-04-09 15:45:55 +0530

    data is saved in csv

--- qrcode/main.py
+++ qrcode/main.py
@@ -1,37 +1,42 @@
 # ToDo1
 # generate a qrcode for an employee having its credentials
 # import qrcode
-# img = qrcode.make('Emp_id: 123456, Name: John, date: 09-04-2025')
-# type(img)
+#
+# json_data = {
+#     'Emp_id': 123456,
+#     'Name': 'John',
+#     'date': '09-04-2025',
+# }
+# img = qrcode.make(str(json_data))
+# print(type(img))    #this return <class 'PIL.Image.Image'>
 # img.save("my_qrcode.png")
 
 # to do 2: to read the data through camera and print it
 import cv2
+data = {}
+camera = cv2.VideoCapture(0)
+# this opens the default camera of the system
 
-
-cap = cv2.VideoCapture(0)
 detector = cv2.QRCodeDetector()
-last_data = ""
-
-
+# this will detect the qr from the camera in real time
 while True:
-    _, frame = cap.read()
-    data, bbox, _ = detector.detectAndDecode(frame)
-    if data and data != last_data:
-        print(data)
-        last_data = data
-
-
-    cv2.imshow("QR code detector", frame)
-    key = cv2.waitKey(1)
-
-    cv2.imshow("QRCODEscanner", frame )
+    _,image = camera.read()
+    data, bbox, straight_qrcode = detector.detectAndDecode(image)
+    print(data)
+    if data:
+        a = data
+        break
+    cv2.imshow("QRCODEscanner", image)
     if cv2.waitKey(1) == ord("q"):
         break
 
-
-cap.release()
+camera.release()
 cv2.destroyAllWindows()
-fs =  open('qrData.csv', 'a')
-fs.write(data+'\n')
-fs.close()
+
+import csv
+fields = ['Emp_id', 'Name', 'date']
+
+
+with open("qrData.csv", "a") as f:
+    f.write(data)
+    f.write("\n")
